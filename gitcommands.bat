79febef184c27241f251981293ed48ac891ed63d
itzmatt019 <itzmatt019@gmail.com>
2023-10-01 07:15:15 -0700

    feat(add): Initial Commit

---
+++ gitcommands.bat
@@ -0,0 +1,7 @@
+echo "# itzmatt-portfolio" >> README.md
+git init
+git add .
+git commit -m %1
+git branch -M main
+git remote add origin https://github.com/ItzMatt019/itzmatt-portfolio.git
+git push -u origin main
